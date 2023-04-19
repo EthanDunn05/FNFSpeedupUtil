b2723635ccec5ff0c0824fa1278934d42ad6621e
Ethan Dunn <ethan05dunn@gmail.com>
2023-04-19 13:25:05 -0500

    Maybe working ffmpeg downloading

--- FNFSpeedupUtil/Program.cs
+++ FNFSpeedupUtil/Program.cs
@@ -1,24 +1,30 @@
 ﻿using System.IO.Abstractions;
+using System.Reflection;
+using FNFSpeedupUtil.Console;
 using FNFSpeedupUtil.MenuSystem;
 using FNFSpeedupUtil.MenuSystem.Pages;
 using Spectre.Console;
 using Xabe.FFmpeg;
-
-LoadFfmpeg();
+using Xabe.FFmpeg.Downloader;
 
 var fileSystem = new FileSystem();
 
 var menu = new Menu(AnsiConsole.Console);
+LoadFfmpeg();
+
 menu.ChangePage(new MainPage(fileSystem));
 
-async void LoadFfmpeg()
+void LoadFfmpeg()
 {
-    // Find ffmpeg
-    var path = Environment.GetEnvironmentVariable("PATH");
-    var paths = path.Split(Path.PathSeparator);
-    var ffmpegPath = paths.FirstOrDefault(
-        s => s.Contains("ffmpeg", StringComparison.OrdinalIgnoreCase)
-    );
+    var ffmpegPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+    if (!File.Exists(ffmpegPath + "/ffmpeg") || !File.Exists(ffmpegPath + "/ffprobe"))
+    {
+        menu.Console.Notification("Downloading ffmpeg").Open(() =>
+        {
+            FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official, ffmpegPath).Wait();
+        });
+    }
 
     FFmpeg.SetExecutablesPath(ffmpegPath);
 }
