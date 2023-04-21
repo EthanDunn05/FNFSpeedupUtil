5ec13051409a3769056bead82a56dcd3e3f0e0d7
Ethan Dunn <Ethan05Dunn@gmail.com>
2023-04-20 21:19:24 -0500

    Add ffmpeg downloading with a good ui

---
+++ FNFSpeedupUtil/MenuSystem/Pages/FFmpegDownloadPage.cs
@@ -0,0 +1,44 @@
+﻿using Spectre.Console;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Downloader;
+
+namespace FNFSpeedupUtil.MenuSystem.Pages;
+
+public class FFmpegDownloadPage : IPage
+{
+    private string FFmpegPath { get; }
+
+    public FFmpegDownloadPage(string ffmpegPath)
+    {
+        FFmpegPath = ffmpegPath;
+    }
+
+    public void Render(Menu menu)
+    {
+        menu.Console.MarkupLine("[green]FFmpeg is being downloaded. This will only happen once.[/]");
+
+        var progress = new Progress<ProgressInfo>();
+        menu.Console.Progress()
+            .Columns(
+                new TaskDescriptionColumn(),
+                new SpinnerColumn(),
+                new ProgressBarColumn(),
+                new DownloadedColumn(),
+                new RemainingTimeColumn())
+            .StartAsync(async ctx =>
+            {
+                var downloaded = ctx.AddTask("Downloading FFmpeg");
+
+                // Start the download and add an event to change the progress bar when there is progress
+                var task = FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official, FFmpegPath, progress);
+                progress.ProgressChanged += (_, info) =>
+                {
+                    // Math.Max because the progress goes backwards for some reason
+                    downloaded.Value = Math.Max(info.DownloadedBytes, downloaded.Value);
+                    downloaded.MaxValue = info.TotalBytes;
+                };
+
+                await task;
+            }).Wait();
+    }
+}
--- FNFSpeedupUtil/Program.cs
+++ FNFSpeedupUtil/Program.cs
@@ -1,30 +1,19 @@
 ﻿using System.IO.Abstractions;
 using System.Reflection;
-using FNFSpeedupUtil.Console;
 using FNFSpeedupUtil.MenuSystem;
 using FNFSpeedupUtil.MenuSystem.Pages;
 using Spectre.Console;
 using Xabe.FFmpeg;
-using Xabe.FFmpeg.Downloader;
 
 var fileSystem = new FileSystem();
 
 var menu = new Menu(AnsiConsole.Console);
-LoadFfmpeg();
+
+// Load FFmpeg
+var ffmpegPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+if (!File.Exists(ffmpegPath + "/ffmpeg") || !File.Exists(ffmpegPath + "/ffprobe"))
+    menu.ChangePage(new FFmpegDownloadPage(ffmpegPath));
+
+FFmpeg.SetExecutablesPath(ffmpegPath);
 
 menu.ChangePage(new MainPage(fileSystem));
-
-void LoadFfmpeg()
-{
-    var ffmpegPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-    if (!File.Exists(ffmpegPath + "/ffmpeg") || !File.Exists(ffmpegPath + "/ffprobe"))
-    {
-        menu.Console.Notification("Downloading ffmpeg").Open(() =>
-        {
-            FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official, ffmpegPath).Wait();
-        });
-    }
-
-    FFmpeg.SetExecutablesPath(ffmpegPath);
-}
